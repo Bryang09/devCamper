54304652069fa985f6e8c5d2f7e111d702ed1fe7
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2020-01-16 18:47:13 -0600

    Connected MongoDB

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 
-// ROUTE FILES
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 // LOAD ENV VARS
 
 dotenv.config({ path: "./config/config.env" });
+
+// CONNECT TO DATABASE
+connectDB();
+
+// ROUTE FILES
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -21,4 +26,11 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Listening on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Listening on port ${PORT}`));
+
+// HANDLE UNHANDLED PROMISE REJECTIONS
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //   CLOSE SERVER & EXIT PROCCESS
+  server.close(() => process.exit(1));
+});

6f6c61c91cb49b7a92f6e6226502b0880ea5cc6c
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2020-01-16 18:52:04 -0600

    Added Colors package for terminal

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 
 const connectDB = require("./config/db");
 
@@ -26,11 +27,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Listening on port ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.cyan.bold
+  )
+);
 
 // HANDLE UNHANDLED PROMISE REJECTIONS
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   //   CLOSE SERVER & EXIT PROCCESS
   server.close(() => process.exit(1));
 });

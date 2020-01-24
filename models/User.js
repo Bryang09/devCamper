539bdf205ae3c57f3abf6bb205673a1f3f864d42
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2020-01-23 19:01:58 -0600

    Added jsonwebtoken

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -41,4 +42,10 @@ UserSchema.pre("save", async function(next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// SIGN JWT AND RETURN
+UserSchema.methods.getSignedJWTToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
 module.exports = mongoose.model("User", UserSchema);

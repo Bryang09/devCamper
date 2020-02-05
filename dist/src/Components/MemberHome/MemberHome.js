d375c40db3f9caa04fdf7337f177b5a80a86af6f
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2020-02-04 19:15:26 -0600

    Get all Bootcamps route display

--- dist/src/Components/MemberHome/MemberHome.js
+++ dist/src/Components/MemberHome/MemberHome.js
@@ -6,13 +6,15 @@ import { BASE_URL } from "../../keys";
 
 import "./MemberHome.scss";
 import Sorting from "./Sorting/Sorting";
+import Results from "./Results/Results";
 
 export class MemberHome extends Component {
   state = {
     photo: null,
     _id: null,
     name: null,
-    email: null
+    email: null,
+    bootcamps: []
   };
 
   componentDidMount = () => {
@@ -27,21 +29,33 @@ export class MemberHome extends Component {
     })
       .then(res => {
         const { photo, _id, name, email } = res.data.data;
-        this.setState({ photo, _id, name, email });
+        this.setState({ photo, _id, name, email }, this.getBootcamps);
         console.log(res);
       })
       .catch(err => console.log({ err }));
   };
+
+  getBootcamps = () => {
+    axios({
+      method: "get",
+      url: `${BASE_URL}/api/v1/bootcamps`
+    })
+      .then(res =>
+        this.setState({ bootcamps: res.data.data }, console.log(res.data))
+      )
+      .catch(err => console.log({ err }));
+  };
+
   render() {
-    const { photo, _id, name, email } = this.state;
-    console.log(photo, _id, name, email);
+    const { photo, _id, name, email, bootcamps } = this.state;
+    console.log(bootcamps);
     return (
       <div className="memberLanding">
         <div className="sort">
           <Sorting photo={photo} _id={_id} name={name} email={email} />
         </div>
         <div className="results">
-          <h1>Members</h1>
+          <Results bootcamps={bootcamps} />
         </div>
       </div>
     );
---
+++ dist/src/Components/MemberHome/Results/Results.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+import "./Results.scss";
+
+import { BASE_URL } from "../../../keys.js";
+
+const Results = props => {
+  const { bootcamps } = props;
+
+  const singleBootcamp = bootcamps.map(bootcamp => {
+    const {
+      photo,
+      name,
+      description,
+      website,
+      phone,
+      averageCost,
+      averageRating,
+      _id
+    } = bootcamp;
+
+    return (
+      <div className="singleBootcamp" key={_id}>
+        <div className="singleBootcampContainer">
+          <div className="bootcampImageContainer">
+            <div
+              className="bootcampImage"
+              style={{
+                backgroundImage: `url(${BASE_URL}/uploads/${photo})`
+              }}
+            ></div>
+          </div>
+
+          <div className="bootcampInfo">
+            <span className="name">
+              <h3>{name}</h3>
+              <h4>{averageRating}</h4>
+            </span>
+
+            <h5>{website}</h5>
+            <h5>{phone}</h5>
+
+            <p>{description}</p>
+          </div>
+        </div>
+        <hr />
+      </div>
+    );
+  });
+
+  return (
+    <div className="resultsContainer">
+      <div className="header">
+        <h2>Bootcamps</h2>
+      </div>
+      <div className="content">{singleBootcamp}</div>
+      <div className="pagination"></div>
+    </div>
+  );
+};
+
+export default Results;

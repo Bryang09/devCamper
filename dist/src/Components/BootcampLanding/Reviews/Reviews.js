918df3e221f73daec30227f6f0b61d90fb1843e2
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2020-02-22 10:24:09 -0600

    Font Awesome Added

--- dist/src/Components/BootcampLanding/Reviews/Reviews.js
+++ dist/src/Components/BootcampLanding/Reviews/Reviews.js
@@ -2,24 +2,27 @@ import React from "react";
 
 import "./Reviews.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Reviews = props => {
   const { reviews } = props;
 
   console.log(reviews);
 
-  const review = reviews.map(res => {
+  const review = reviews.map((res, i) => {
     const { title, rating, text } = res;
     return (
-      <div className="review">
+      <div className="review" key={i}>
         <div className="userInfo">
           <div className="userPicture"></div>
-          <div className="userName"></div>
+          <div className="userName">
+            <h4>Bryan Gonzalez</h4>
+          </div>
         </div>
         <div className="reviewContent">
           <div className="container">
             <h4>{title}</h4>
-            <h5>{rating}</h5>
-            <br />
+            <h5>{rating}/10</h5>
             <h6>{text}</h6>
           </div>
         </div>
